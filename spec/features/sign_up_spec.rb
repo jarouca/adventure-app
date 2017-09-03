bc2415a8edbb5be8a8e3e9e7259499695a72b1c4
Jonathan Arouca <jonathanarouca@gmail.com>
2017-09-03 15:25:56 -0400

    add sign_up_spec

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'user creates an account' do
+  # As an unathenticated user
+  # I want to sign up
+  # So that I can track my progress
+  #
+  # ACCEPTANCE CRITERIA
+  # * I must specify a valid email adress
+  # * I must specify and confirm a password
+  # * If I do not perform the above I get an error message
+  # * If I specify valid information, I register my account and am authenticated
+
+  scenario 'specifying valid and required information' do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: 'password'
+    fill_in "Password Confirmation", with: 'password'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("Welcome!")
+    expect(page).to have_content("Sign Out")
+  end
+
+  scenario 'required information is not supplied' do
+
+  end
+
+  scenario 'password confirmation does not match' do
+
+  end
+end
