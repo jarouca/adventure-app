59ea8e68232ed10422cea19442dc977e9bd9e529
Jonathan Arouca <jonathanarouca@gmail.com>
2017-09-05 18:26:00 -0400

    add sign_up_spec unhappy path

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -25,10 +25,24 @@ feature 'user creates an account' do
   end
 
   scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+    click_button 'Sign Up'
 
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content("Sign Out")
   end
 
   scenario 'password confirmation does not match' do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Username", with: "user"
+    fill_in "Email", with: "user@example.com"
+    fill_in "user_password", with: 'password'
+    fill_in "Password Confirmation", with: 'passwors'
+    click_button 'Sign Up'
 
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content("Sign Out")
   end
 end
