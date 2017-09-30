d88f7bcb48a241d5e4b26b7b5c49944b20e3c2b0
Jonathan Arouca <jonathanarouca@gmail.com>
2017-09-29 22:42:02 -0400

    add user edits character spec

---
+++ spec/features/user_edits_character_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'user creates a character' do
+  # As an authenticated user
+  # I want to edit my character
+  # So I can make changes to the character's attributes
+  #
+  # Acceptance criteria:
+  # - I must be given notification that my character edit was successful
+  # - if I don't properly fill out the form I must receive error messages
+
+  scenario 'user successfully edits character' do
+
+  end
+
+  scenario 'user fails to provide the required information'
+  end
+end
