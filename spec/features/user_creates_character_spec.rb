66ca84edfe526905032398ec18b8b4bcc8c4c217
Jonathan Arouca <jonathanarouca@gmail.com>
2017-10-03 15:18:20 -0400

    add character model

--- spec/features/user_creates_character_spec.rb
+++ spec/features/user_creates_character_spec.rb
@@ -8,11 +8,12 @@ feature 'user creates a character' do
   # Acceptance criteria:
   # - I must be given notification that my character creation was successful
   # - if I don't properly fill out the form I must receive error messages
+  let!(:user) { FactoryGirl.create(:user) }
 
   scenario 'user successfully creates character' do
     login_as(user, :scope => :user)
     visit root_path
-    click_link 'Create Character'
+    click_link 'Create A Character'
     fill_in 'Name', with: 'Conan'
     page.select "6'5", from: "Height"
     page.select "225", from: "Weight"
