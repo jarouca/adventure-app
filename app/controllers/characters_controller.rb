dab06f0538313b0915bbf1e97f8738a3ac3624b1
Jonathan Arouca <jonathanarouca@gmail.com>
2017-10-09 17:21:46 -0400

    edit new character form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,5 +4,15 @@ class CharactersController < ApplicationController
   end
 
   def new
-  end 
+    @height_options = []
+    feet = (1..8).to_a
+    feet.each do |height|
+      count = 1
+      while count < 12 do
+        @height_options << height.to_s + "&#39;" + count.to_s + "&#34;"
+        count += 1
+      end
+
+    end
+  end
 end
